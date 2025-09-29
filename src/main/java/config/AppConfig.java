a74a93e2f5ec500787ad3eb9dd8870e3df949933
hamza2024web <hamzaakroubi1528@gmail.com>
2025-09-29 15:00:13 +0100

    create the currency class to convert the amount depend on the devise

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -33,5 +33,4 @@ public class AppConfig {
         return amlThreshold;
     }
 
-
 }
--- src/main/java/util/CurrencyConverter.java
+++ src/main/java/util/CurrencyConverter.java
@@ -1,4 +1,15 @@
 package util;
 
+import config.AppConfig;
+import domain.Enums.Currency;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class CurrencyConverter {
+    public static BigDecimal convert (BigDecimal amount, Currency from, Currency to){
+        if (from == to) return amount.setScale(2, RoundingMode.HALF_UP);
+        BigDecimal rate = AppConfig.getInstance().getRate(from.name(), to.name());
+        return amount.multiply(rate).setScale(2,RoundingMode.HALF_UP);
+    }
 }
